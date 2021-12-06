3a8e24bed3df9f24995039d0cdf56bd0482cc606
Damian <d.janas13@gmail.com>
2021-12-06 14:54:14 +0100

    Add files via upload

---
+++ code_snippets/X_2.cs
@@ -0,0 +1,15 @@
+#r "Newtonsoft.Json"
+
+using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Newtonsoft.Json;
+
+public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
+{
+    log.LogInformation("C# HTTP trigger function processed a request.");
+	
+	Convert.ToInt32("test");
+
+    return new OkObjectResult("Hello from Azure Funtion!");
+}
