07a039635df5599293834a505c14740b545d7b23
Damian <d.janas13@gmail.com>
2021-12-06 14:54:43 +0100

    Update X_2.cs

--- code_snippets/X_2.cs
+++ code_snippets/X_2.cs
@@ -9,7 +9,7 @@ public static async Task<IActionResult> Run(HttpRequest req, ILogger log)
 {
     log.LogInformation("C# HTTP trigger function processed a request.");
 	
-	Convert.ToInt32("test");
+    Convert.ToInt32("test");
 
     return new OkObjectResult("Hello from Azure Funtion!");
 }
