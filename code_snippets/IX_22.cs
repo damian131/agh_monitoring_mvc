128dd9b01c926cec84ef4a63074a0cc46ed925bf
Damian <d.janas13@gmail.com>
2023-12-11 21:59:02 +0100

    Update IX_22.cs

--- code_snippets/IX_22.cs
+++ code_snippets/IX_22.cs
@@ -1,9 +1,9 @@
 [HttpGet]
-public Task<ActionResult<string>> GetText()
+public async Task<ActionResult<string>> GetText()
 {
 	var functionUrl = "<func-url>";
-	var cleint = new HttpClient();
+	var client = new HttpClient();
 	var response = await client.GetAsync(functionUrl);
 	var text = await response.Content.ReadAsStringAsync();
-    return new OkObjectResult("Success!");
+	return new OkObjectResult("Success!");
 }
