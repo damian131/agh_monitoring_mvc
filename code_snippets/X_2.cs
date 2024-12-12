baeb319b0f90f8bca26818dd56a82e3d42f36ea2
Damian <d.janas13@gmail.com>
2024-12-12 01:13:50 +0100

    Update X_2.cs

--- code_snippets/X_2.cs
+++ code_snippets/X_2.cs
@@ -1,13 +1,14 @@
-[Function("Function1")]
-public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
-{
-	_logger.LogInformation("C# HTTP trigger function processed a request
-	
-	Convert.ToInt32("test");
+        [Function("Function1")]
+        public async Task<HttpResponseData> Run([HttpTrigger(AuthorizationLevel.Function, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request");
+        
 
-	var response = req.CreateResponse(HttpStatusCode.OK);
-	response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
-	await response.WriteStringAsync("Hello from Azure Funtion!");
+            Convert.ToInt32("test");
 
-	return response;
-}
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+            await response.WriteStringAsync("Hello from Azure Funtion!");
+
+            return response;
+        }
